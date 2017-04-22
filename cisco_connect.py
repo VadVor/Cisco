6a3d542475f0908f9b27185f0d562d5603403ae6
VadVor <vadimvoronov192@gmail.com>
2017-04-22 16:14:47 +0300

    Project for Cisco devices

---
+++ cisco_connect.py
@@ -0,0 +1,16 @@
+import crassh
+
+
+routers = ["13.79.232.71"]
+username = "user_csr1000"
+password = "Piligrimm_01"
+
+for device in routers:
+    hostname = crassh.connect(device, username, password)
+    output = crassh.send_command("show run | inc snmp-server community", hostname)
+    crassh.disconnect()
+words = output.split()
+for x in words:
+    if x == "public":
+        print("DANGER: Public SNMP Community set on %s [%s]" % (hostname, device))
+
