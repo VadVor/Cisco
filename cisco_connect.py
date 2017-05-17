c9bba8f69d707758281f277e1665025b3593fb63
VadVor <vadimvoronov195@gmail.com>
2017-05-17 16:49:00 +0300

    Update cisco_connect.py

--- cisco_connect.py
+++ cisco_connect.py
@@ -3,7 +3,7 @@ import crassh
 
 routers = ["13.79.232.71"]
 username = "user_csr1000"
-password = "Piligrimm_01"
+password = "********mm_01"
 
 for device in routers:
     hostname = crassh.connect(device, username, password)
